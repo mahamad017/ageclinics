40347f806d91d00180fe24715acbad3e6352c769
mahamad017 <khermahamad7@gmail.com>
2024-06-15 15:31:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const { default: Server } = require("next/dist/server/base-server");
+
+let doctors = document.querySelectorAll(".team");
+let count = 0;
+
+doctors.forEach(function(card,index){
+    card.style.left=`${index*100}%`
+})
+function myFun(){
+    doctors.forEach(function(curValue)
+{
+    curValue.style.transform=`translateX(-${count * 100}%)`
+})
+}
+setInterval(function(){
+    count++;
+    if(count == doctors.length){
+        count=0;
+    }
+    myFun()
+}, 3000)
+  
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    });
+}
+
